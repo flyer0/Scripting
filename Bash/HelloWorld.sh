ce58784d81ac7652a65a4dc5319adaabcdd53df7
flyer0 <flyer0@github.com>
2019-08-18 14:36:10 +0200

    Scripting

---
+++ Bash/HelloWorld.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "Hello  World"
+date +"Today is %A, a day in %B"
+echo "Home "$PATH
+echo "UID "$UID
+echo "PWD "$PWD
+echo "Home "$HOME
+echo "PS1 " $PS1
+echo "USER "$USER
+exit
