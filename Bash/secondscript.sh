d2451abd4a33d57c51b865bbfd775f81a182b2d0
flyer0 <flyer0@github.com>
2019-08-23 11:12:19 +0200

    finished

--- Bash/secondscript.sh
+++ Bash/secondscript.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/bash
 
-echo "Das Arbeitsverzeichnis der $SHELL ist $(pwd)"
+#Funktionstest
+
+
+chkfile(){
+    echo "File $1 is"
+    [ -r $1 ] && echo "readable"
+    [ -x $1 ] && echo "executable"
+}
+
+chkfile /ur/bin/emacs
+
+#Test der Variablen
+#echo "Das Arbeitsverzeichnis der $SHELL ist $(pwd)"
 exit
