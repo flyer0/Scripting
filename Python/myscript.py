9959073079b288d6c0c3fee23da3e072443eb804
flyer0 <flyer0@github.ch>
2019-09-18 18:49:29 +0200

    Phython learning

--- Python/myscript.py
+++ Python/myscript.py
@@ -1,3 +1,14 @@
-x = 1;
-y = 2;
-print(x+y)
+import random
+
+a=[]
+n = 100
+
+
+a=[random.randint(0,9) for i in range(n)]
+
+avg = sum(a)/len(a)
+e = [(n.samples.count(n)) for n in range(10)]
+
+print(n)
+
+
