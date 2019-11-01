d63e6f4745120a299c17264da8a60a76ef5ee45a
flyer0 <flyer0@github.ch>
2019-11-01 10:57:14 +0100

    regex test

--- Python/aufgabe.py
+++ Python/aufgabe.py
@@ -1,8 +1,31 @@
+#Split String
 line = ' Water : 9.876 : at 20°C'
 
-print(line.split(':'));
+#print(line.split(':'));
 
 name, value, desc = line.split(':')
-print(value);
+#rint(value);
 
+#Open File
+f=open('phoentic.dat', encoding='utf-8')
+#print(f.read())
+f.close()
+
+#Subprocess, Commands on OS
+import subprocess
+#ret=subprocess.call(["ping","-c1","8.8.8.8"])
+
+#Functions
+number = 1234;
+def reverse(number):
+    """reverses the digit sequence of a number
+    Args: number: the number to be reversed
+    """
+    s = list(str(number))[::-1]
+    return ''.join(s)
+
+print(reverse(number))
+
+#print(reverse.__doc__)
+#help(reverse)
 
