d65a4cfb411326a3b4500eacfbfc3b04e0fd15a4
flyer0 <flyer0@github.ch>
2019-11-08 22:14:37 +0100

    Iterate Array

--- Bash/checkdb.sh
+++ Bash/checkdb.sh
@@ -5,50 +5,69 @@
 
 #-------------------ROLE---------------------------------------------------------------
 #Check if Role is in DB
-echo ${R_NAME[1]}
-role=$(mysql -D userreg -u root  -se "select name FROM role WHERE name = '$R_NAME'")
+
+for ((i=0;i<${#R_NAME[@]};i++));do
+
+    role=$(mysql -D userreg -u root  -se "select name FROM role WHERE name = '${R_NAME[$i]}'")
+
 if [ -z "$role" ]
 then
 mysql -u root<<EOFMYSQL
 use userreg;
-INSERT INTO role (description,name) VALUE ("$R_DESCRIPTION","$R_NAME");
+INSERT INTO role (description,name) VALUE ("${R_DESCRIPTION[$i]}","${R_NAME[$i]}");
 EOFMYSQL
-  echo "Role $R_NAME added to DB!"
+  echo "Role ${R_NAME[$i]} added to DB!"
+  
 else
- echo "Role already in DB...";
+  echo "Role already in DB...";
+
 fi
+
+done
+
 #-------------------USER---------------------------------------------------------------
-user=$(mysql -D userreg -u root  -se "select username FROM app_user WHERE username= '$U_UNAME'")
+
+for ((i=0;i<${#U_UNAME[@]};i++));do
+user=$(mysql -D userreg -u root  -se "select username FROM app_user WHERE username= '${U_UNAME[$i]}'")
+
 if [ -z "$user" ]
 then
 mysql -u root<<EOFMYSQL
 use userreg;
-INSERT INTO app_user (account_expired,email,first_name,last_name,password,username) VALUE ($U_EXPIRE,"$U_EMAIL","$U_FIRSTNAME","$U_NAME","$U_PASSWORD","$U_UNAME");
+INSERT INTO app_user (account_expired,email,first_name,last_name,password,username) VALUE (${U_EXPIRE[$i]},"${U_EMAIL[$i]}","${U_FIRSTNAME[$i]}","${U_NAME[$i]}","${U_PASSWORD[$i]}","${U_UNAME[$i]}");
 EOFMYSQL
-  echo "User $U_FIRSTNAME $U_NAME added to DB!"
+  echo "User ${U_FIRSTNAME[$i]} ${U_NAME[$i]} added to DB!"
 else
- echo "User already in DB...";
+  echo "User already in DB...";
 fi
 
+done
 
 #-------------------ADD ROLE TO USER----------------------------------------------------
 
 
-roleid=$(mysql -D userreg -u root  -se "select id FROM role WHERE name = '$U_ROLE'")
-unameid=$(mysql -D userreg -u root  -se "select id FROM app_user WHERE username = '$U_UNAME'")
+for ((i=0;i<${#U_UNAME[@]};i++));do
+ 
+roleid=$(mysql -D userreg -u root  -se "select id,name FROM role WHERE name = '${U_ROLE[$i]}'")
+uname=$(mysql -D userreg -u root  -se "select id,username FROM app_user WHERE username = '${U_UNAME[$i]}'")
 
-rolename=$(mysql -D userreg -u root  -se "select name FROM role WHERE name = '$U_ROLE'")
-uname=$(mysql -D userreg -u root  -se "select last_name FROM app_user WHERE username = '$U_UNAME'")
+read var1 var2 <<< $uname
+read rvar1 rvar2 <<< $roleid
 
-echo $roleid
-echo $unameid
 
-if [[ -n "$roleid" && -n "$unameid" ]] ; then
+id=$(mysql -D userreg -u root  -se "select user_id,role_id FROM user_role WHERE role_id = '$rvar1' AND user_id='$var1'")
+
+
+echo $id
+
+if [ -z "$id" ] ; then
 mysql -u root<<EOFMYSQL
 use userreg;
-INSERT INTO user_role (user_id,role_id) VALUE ("$unameid","$roleid");
+INSERT INTO user_role (user_id,role_id) VALUE ($var1,$rvar1);
 EOFMYSQL
-  echo "User $uname has $rolename rights!"
+   echo "User $var2 has $rvar2 rights!"
 else
- echo "User or role not found in DB...";
+   echo "Role-User already associated!";
 fi
+
+done
--- Bash/config.sh
+++ Bash/config.sh
@@ -2,15 +2,25 @@
 
 #Create Role
 R_NAME=("USER" "ADMIN")
-R_DESCRIPTION="User Rights"
+R_DESCRIPTION=("User Rights" "Admin Rights")
 
 
 
 #Create User
-U_FIRSTNAME="Test"
-U_NAME="User"
-U_UNAME="TU"
-U_EMAIL="test.user@user.ch"
-U_EXPIRE=0
-U_PASS="Welcome01"
-U_ROLE="USER"
+U_FIRSTNAME=("Peter" "Hans")
+U_NAME=("Mueller" "Wurschd")
+U_UNAME=("PEMUE" "HAWU")
+U_EMAIL=("peter.mueller@user.ch" "hans.wurschd@user.ch")
+U_EXPIRE=(0 1)
+U_PASS=("Welcome01" "Welcome02")
+U_ROLE=("USER" "ADMIN")
+
+if [ -n "${U_ROLE[$i]}" ] ; then
+mysql -u root<<EOFMYSQL
+use userreg;
+INSERT INTO user_role (user_id,role_id) VALUE ("${uname[$i]}","${roleid[$i]}");
+EOFMYSQL
+   echo "User ${uname[$i+1]} has ${roleid[$i]} rights!"
+else
+   echo "User or role not found in DB...";
+fi
